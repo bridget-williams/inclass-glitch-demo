d3697296ca22f79d00debb2783c10281444f5b01
Glitch (bridget-williams-inclass-glitch-demo) <none>
2019-02-13 18:09:07 +0000

    ✈️🌷 Checkpoint ./server.js:3109120/169

--- server.js
+++ server.js
@@ -82,12 +82,16 @@ app.get('/category-playlists', function (request, response) {
   
   countries.forEach((country)=>{
   console.log(country)
+    console.log(new Date());
     spotifyApi.getPlaylistsForCategory('roots', { country: country.code, limit : 10})
     
     .then((data) =>{
     
     // Send the list of playlists
-    response.send(data.body.playlists);
+    // response.send(data.body.playlists);
+      
+      country.data = data;
+      
     
   }, function(err) {
     console.error(err);
